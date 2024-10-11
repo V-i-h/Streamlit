737895c8475fe28a92841b7854f161c2dd9f0e5b
Vitória <vitoriasilvan321@gmail.com>
2024-10-11 11:46:51 -0300

    11/10

--- aula0.py
+++ aula0.py
@@ -5,22 +5,36 @@ import pandas as pd
 import plotly.express as px
 
 
+def formatar_valor(valor):
+    if valor >= 1000000:
+        return f"{valor/1000000:.2f} milhões"
+    else :
+        return f"{valor / 1000:.2f} mil"
+    
+        
 st.title("DASHBOARD DE VENDAS:shopping_trolley:")
-url = "https://labdados.com/produtos?regiao=norte&ano=2022"
+url = "https://labdados.com/produtos"
+
 response = requests.get(url)
-
-# json
-# dicionario
-
 df = pd.DataFrame.from_dict(response.json())
 if st.button("todos"):
+    receita = df['Preço'].sum()
+    st.metric("Receita",formatar_valor(receita))
+    linhas = df.shape[0]
+    st.metric("Quantidade de vendas (linhas)",formatar_valor(linhas))
     st.dataframe(df)
     st.balloons()
-    st.metric("receita",df['Preço'].sum())
-    st.metric("Quantidade de vendas (linhas)",df.shape[0])
-    st.metric("Quantidade de variaveis (colunas)",df.shape[1])
-    st.dataframe(df)
-    st.snow
+    st.snow()
     
 else:
     st.write("Clique no botão todos")
+    
+    
+"""    
+def formata_numero(valor,prefixo = ''):
+    for unidade in ['', 'mil']:
+        if valor < 1000:
+            return f'{prefixo} {valor:.2f} {unidade}'
+        valor /= 1000
+    return f'{prefixo} {valor:.2f} milhões'
+"""
